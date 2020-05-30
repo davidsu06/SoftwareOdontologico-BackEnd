84f315ef18907f6a845bced22d10115784707f2c
Jojansantia <jojansantia0317@gmail.com>
2020-05-30 07:11:30 -0500

    Ajustes package.json

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.use('/api/tratamientos', require('./routes/tratamiento'));
 app.use('/api/auth', require('./routes/auth'));
 
 // Arrancar la app
-app.listen(PORT,()=>{
-    console.log(`El servidor está corriedo en el puerto ${PORT}`);
-})
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
